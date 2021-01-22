frida/vala

commit 6e7be51cb5455e9c5c721d4ba5121a6ab04673dd

vala: Don't allow assigning GtkChild fields/properties
These are handled exclusively by GtkBuilder

See https://gitlab.gnome.org/GNOME/vala/issues/1121

vala/valaunaryexpression.vala
@@ -229,6 +229,11 @@ public class Vala.UnaryExpression : Expression {
 				Report.error (source_reference, "ref and out method arguments can only be used with fields, parameters, local variables, and array element access");
 				return false;
 			}
+			if (inner.symbol_reference.get_attribute ("GtkChild") != null) {
+				error = true;
+				Report.error (source_reference, "Assignment of [GtkChild] `%s' is not allowed", inner.symbol_reference.get_full_name ());
+				return false;
+			}
 			break;
 		default:
 			error = true;

tests/Makefile.am
@@ -709,6 +709,10 @@ TESTS = \
 	gir/property-non-readable.test \
 	gir/symbol-type-csuffix.test \
 	gir/union.test \
+	gtktemplate/gtkchild-field-assignment.test \
+	gtktemplate/gtkchild-field-out-assignment.test \
+	gtktemplate/gtkchild-field-ref-assignment.test \
+	gtktemplate/gtkchild-property-assignment.test \
 	annotations/deprecated.vala \
 	annotations/description.vala \
 	annotations/noaccessormethod.test \
@@ -1152,6 +1156,7 @@ LINUX_TESTS += \
 endif
 
 EXTRA_DIST = \
+	gtktemplate/tests-extra-environment.sh \
 	linux/tests-extra-environment.sh \
 	nullability/tests-extra-environment.sh \
 	posix/tests-extra-environment.sh \

tests/gtktemplate/gtkchild-field-assignment.test
@@ -1,0 +1,14 @@
+Invalid Code
+
+[GtkTemplate (ui = "/org/example/gtktemplate.ui")]
+public class GtkTemplate : Gtk.ApplicationWindow {
+	[GtkChild]
+	public unowned Gtk.Button button0;
+
+	void foo () {
+		button0 = new Gtk.Button ();
+	}
+}
+
+void main () {
+}

tests/gtktemplate/gtkchild-field-out-assignment.test
@@ -1,0 +1,17 @@
+Invalid Code
+
+[GtkTemplate (ui = "/org/example/gtktemplate.ui")]
+public class GtkTemplate : Gtk.ApplicationWindow {
+	[GtkChild]
+	public unowned Gtk.Button button0;
+
+	void foo () {
+		bar (out button0);
+	}
+
+	void bar (out unowned Gtk.Button b) {
+	}
+}
+
+void main () {
+}

tests/gtktemplate/gtkchild-field-ref-assignment.test
@@ -1,0 +1,17 @@
+Invalid Code
+
+[GtkTemplate (ui = "/org/example/gtktemplate.ui")]
+public class GtkTemplate : Gtk.ApplicationWindow {
+	[GtkChild]
+	public unowned Gtk.Button button0;
+
+	void foo () {
+		bar (ref button0);
+	}
+
+	void bar (ref unowned Gtk.Button b) {
+	}
+}
+
+void main () {
+}

tests/gtktemplate/gtkchild-property-assignment.test
@@ -1,0 +1,14 @@
+Invalid Code
+
+[GtkTemplate (ui = "/org/example/gtktemplate.ui")]
+public class GtkTemplate : Gtk.ApplicationWindow {
+	[GtkChild]
+	public unowned Gtk.Button button0 { get; set; }
+
+	void foo () {
+		button0 = new Gtk.Button ();
+	}
+}
+
+void main () {
+}

tests/gtktemplate/gtktemplate.vala
@@ -1,7 +1,7 @@
 [GtkTemplate (ui = "/org/example/gtktemplate.ui")]
 public class GtkTemplate : Gtk.ApplicationWindow {
 	[GtkChild]
-	public Gtk.Button button0 { get; set; }
+	public Gtk.Button button0 { get; }
 
 	[GtkChild (internal = true)]
 	public Gtk.Button button1;

tests/gtktemplate/tests-extra-environment.sh
@@ -1,0 +1,2 @@
+PACKAGES="gtk+-3.0"
+VALAFLAGS="--gresources ${abs_srcdir}/gtktemplate/gtktemplate.gresource.xml"

commit c2a3f42f71caae18dfad837a96a901531cce7938

vala: Fix crash in UnaryExpression.check()
When `inner.symbol_reference` is null, which is the case in the
following code from libgee's concurrentset.vala:

    res = search_helper<G> (cmp, key, ref prev._iter[i], ...
                                          ^^^^^^^^^^^^^
                                          |
                                          `inner`, where its
                                          `symbol_reference`
                                          is null

This seems to be a regression introduced in 6e7be51.

vala/valaunaryexpression.vala
@@ -229,9 +229,10 @@ public class Vala.UnaryExpression : Expression {
 				Report.error (source_reference, "ref and out method arguments can only be used with fields, parameters, local variables, and array element access");
 				return false;
 			}
-			if (inner.symbol_reference.get_attribute ("GtkChild") != null) {
+			var r = inner.symbol_reference;
+			if (r != null && r.get_attribute ("GtkChild") != null) {
 				error = true;
-				Report.error (source_reference, "Assignment of [GtkChild] `%s' is not allowed", inner.symbol_reference.get_full_name ());
+				Report.error (source_reference, "Assignment of [GtkChild] `%s' is not allowed", r.get_full_name ());
 				return false;
 			}
 			break;