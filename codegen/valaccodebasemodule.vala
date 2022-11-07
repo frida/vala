frida/vala

commit be3c13f872d11e49218c5cd6ca11aa72dd60719e

codegen: Cast given default-value of struct with possible member initializer
Fixes https://gitlab.gnome.org/GNOME/vala/issues/1272

codegen/valaccodebasemodule.vala
@@ -6544,7 +6544,11 @@ public abstract class Vala.CCodeBaseModule : CodeGenerator {
 		var array_type = type as ArrayType;
 		if (type.type_symbol != null && !type.nullable
 		    && (on_error ? get_ccode_default_value_on_error (type.type_symbol) : get_ccode_default_value (type.type_symbol)) != "") {
-			return new CCodeConstant (on_error ? get_ccode_default_value_on_error (type.type_symbol) : get_ccode_default_value (type.type_symbol));
+		    CCodeExpression val = new CCodeConstant (on_error ? get_ccode_default_value_on_error (type.type_symbol) : get_ccode_default_value (type.type_symbol));
+		    if (st != null && st.get_fields ().size > 0) {
+				val = new CCodeCastExpression (val, get_ccode_name (st));
+			}
+			return val;
 		} else if (initializer_expression && !type.nullable &&
 				   (st != null || (array_type != null && array_type.fixed_length))) {
 			// 0-initialize struct with struct initializer { 0 }

tests/structs/simple-type-default-value.c-expected
@@ -1,0 +1,56 @@
+/* structs_simple_type_default_value.c generated by valac, the Vala compiler
+ * generated from structs_simple_type_default_value.vala, do not modify */
+
+#include <glib.h>
+#include <glib-object.h>
+
+#if !defined(VALA_EXTERN)
+#if defined(_MSC_VER)
+#define VALA_EXTERN __declspec(dllexport) extern
+#elif __GNUC__ >= 4
+#define VALA_EXTERN __attribute__((visibility("default"))) extern
+#else
+#define VALA_EXTERN extern
+#endif
+#endif
+
+typedef struct _Foo Foo;
+#define _vala_assert(expr, msg) if G_LIKELY (expr) ; else g_assertion_message_expr (G_LOG_DOMAIN, __FILE__, __LINE__, G_STRFUNC, msg);
+#define _vala_return_if_fail(expr, msg) if G_LIKELY (expr) ; else { g_return_if_fail_warning (G_LOG_DOMAIN, G_STRFUNC, msg); return; }
+#define _vala_return_val_if_fail(expr, msg, val) if G_LIKELY (expr) ; else { g_return_if_fail_warning (G_LOG_DOMAIN, G_STRFUNC, msg); return val; }
+#define _vala_warn_if_fail(expr, msg) if G_LIKELY (expr) ; else g_warn_message (G_LOG_DOMAIN, __FILE__, __LINE__, G_STRFUNC, msg);
+
+struct _Foo {
+	gint i;
+};
+
+VALA_EXTERN Foo bar (GObject* o);
+static void _vala_main (void);
+
+Foo
+bar (GObject* o)
+{
+	Foo _tmp0_ = (Foo) { .i = 23 };
+	Foo result = (Foo) { .i = 23 };
+	g_return_val_if_fail (G_TYPE_CHECK_INSTANCE_TYPE (o, G_TYPE_OBJECT), (Foo) { .i = 23 });
+	result = _tmp0_;
+	return result;
+}
+
+static void
+_vala_main (void)
+{
+	Foo foo = (Foo) { .i = 23 };
+	Foo _tmp0_ = (Foo) { .i = 23 };
+	foo = _tmp0_;
+	_vala_assert (foo.i == 23, "foo.i == 23");
+}
+
+int
+main (int argc,
+      char ** argv)
+{
+	_vala_main ();
+	return 0;
+}
+

tests/structs/simple-type-default-value.vala
@@ -1,0 +1,14 @@
+[CCode (default_value = "{ .i = 23 }")]
+[SimpleType]
+public struct Foo {
+	int i;
+}
+
+Foo bar (Object o) {
+	return {};
+}
+
+void main () {
+	Foo foo = {};
+	assert (foo.i == 23);
+}

commit 62ee2b101a5e5f37ce2a073fdb36e7f6ffb553d1

codegen: Fix static struct initializers on MSVC
E.g. the following code in libgee:

    private static GLib.Once<ThreadPool<TaskData<G>>> async_pool;

Would generate:

    static GOnce gee_task_data_async_pool = (GOnce) G_ONCE_INIT;

Which MSVC does not like:

    task.c(59): error C2099: initializer is not a constant

However, removing the (redundant) cast is enough to make MSVC happy.

This regression was introduced in be3c13f. To solve this issue we skip
the cast for initializer expressions, as the type is already implied by
the left-hand side of the expression.

codegen/valaccodebasemodule.vala
@@ -6583,7 +6583,7 @@ public abstract class Vala.CCodeBaseModule : CodeGenerator {
 		if (type.type_symbol != null && !type.nullable
 		    && (on_error ? get_ccode_default_value_on_error (type.type_symbol) : get_ccode_default_value (type.type_symbol)) != "") {
 		    CCodeExpression val = new CCodeConstant (on_error ? get_ccode_default_value_on_error (type.type_symbol) : get_ccode_default_value (type.type_symbol));
-		    if (st != null && st.get_fields ().size > 0) {
+		    if (!initializer_expression && st != null && st.get_fields ().size > 0) {
 				val = new CCodeCastExpression (val, get_ccode_name (st));
 			}
 			return val;